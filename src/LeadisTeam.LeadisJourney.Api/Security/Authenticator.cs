c5c4dfcf9a29ff483b4295f0417b09ee6f6f0485
Maxime CHARLES <maxfoow@gmail.com>
2016-06-20 16:16:05 +0200

    improve token generation method

--- src/LeadisTeam.LeadisJourney.Api/Security/Authenticator.cs
+++ src/LeadisTeam.LeadisJourney.Api/Security/Authenticator.cs
@@ -2,6 +2,7 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Principal;
+using LeadisTeam.LeadisJourney.Core.Entities;
 using Microsoft.IdentityModel.Tokens;
 
 namespace LeadisTeam.LeadisJourney.Api.Security
@@ -20,8 +21,11 @@ namespace LeadisTeam.LeadisJourney.Api.Security
 
             // Here, you should create or look up an identity for the user which is being authenticated.
             // For now, just creating a simple generic identity.
-            var identity = new ClaimsIdentity(new GenericIdentity(user, "TokenAuth"),
-                new[] { new Claim("UserId", "1", ClaimValueTypes.Integer) });
+            var identity = new ClaimsIdentity(new GenericIdentity(user.Email, "TokenAuth"),
+                new[] {
+                    new Claim("UserId", user.Id.ToString(), ClaimValueTypes.Integer),
+                    new Claim("UserEmail", user.Email, ClaimValueTypes.Email)
+                });
 
             var securityToken = _jwtSecurityTokenHandler.CreateToken(new SecurityTokenDescriptor {
                 Audience = _tokenAuthOption.Audience,
